7716c4d36fd35320083ab2b79c24fa8c250d349f
genivalc <genivalcandeiadon.neto@gmail.com>
2023-07-24 16:49:30 -0300

    test count

--- angular10ngrx/src/app/product/product.component.ts
+++ angular10ngrx/src/app/product/product.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { Store,select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Increment,  Decrement, Reset } from '../reducers/counter.actions';
+
 
 @Component({
   selector: 'app-product',
@@ -6,5 +11,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
+  count$: Observable<number>;
 
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.pipe(select('count'));
+  }
+
+  increment() {
+    this.store.dispatch(new Increment());
+  }
+
+  decrement() {
+    this.store.dispatch(new Decrement());
+  }
+
+  reset() {
+    this.store.dispatch(new Reset());
+  }
 }
